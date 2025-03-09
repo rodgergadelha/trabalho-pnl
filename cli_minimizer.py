d1b5788ba3c4757cd94af0818564be41924be876
rodger <rodgergadelha21@gmail.com>
2025-03-08 21:43:56 -0300

    refactor: ajustando codigo

--- cli_minimizer.py
+++ cli_minimizer.py
@@ -7,19 +7,22 @@ def minimize_function(func_str, initial_point, method):
         globals = {'np': np}
         locals = {'x': x}
         return eval(func_str, globals, locals)
-        
-    return newton_method(function, initial_point)
+
+    if method == 'Newton':
+        return newton_method(function, initial_point)
+
+    return None, None
 
 def main():
     parser = argparse.ArgumentParser(description="Minimização não linear de uma função multivariável")
     parser.add_argument("func", help="A função matemática a ser minimizada (como uma string, ex: 'x[0]**2 + x[1]**2')")
     parser.add_argument("initial_point", help="Ponto inicial para a minimização, como um array (ex: '[1, 2]')", type=str)
-    #parser.add_argument("method", help="Método de otimização (ex: 'Gradiente', 'Newton')", type=str)
+    parser.add_argument("method", help="Método de otimização (ex: 'Gradiente', 'Newton')", type=str)
     args = parser.parse_args()
     initial_point = np.array(eval(args.initial_point))
-    min_value, min_point = minimize_function(args.func, initial_point, '')
+    min_value, min_point = minimize_function(args.func, initial_point, args.method)
 
-    print(f"Ponto mínimo encontrado: {min_point}")
+    print(f"Ponto ótimo: {min_point}")
     print(f"Valor ótimo: {min_value}")
 
 main()
--- newton.py
+++ newton.py
@@ -2,26 +2,26 @@ import autograd.numpy as np
 from autograd import grad, hessian
 
 def newton_method(f, x0):
-    tol=1e-6
-    max_iter=100
+    tolerance = 1e-6
+    max_iterations = 100
+    gradient_of_f = grad(f)
+    hessian_of_f = hessian(f)
     x = np.array(x0, dtype=float)
-    grad_f = grad(f)
-    hess_f = hessian(f)
-    
-    for _ in range(max_iter):
-        grad_val = grad_f(x)
-        
-        if np.linalg.norm(grad_val) < tol:
+
+    for _ in range(max_iterations):
+        gradient_value = gradient_of_f(x)
+
+        if np.linalg.norm(gradient_value) < tolerance:
             break
-        
-        hess_val = hess_f(x)
-        
+
+        hessian_value = hessian_of_f(x)
+
         try:
-            d = np.linalg.solve(hess_val, -grad_val)
+            d = np.linalg.solve(hessian_value, -gradient_value)
         except np.linalg.LinAlgError:
             print("Método falhou devido a Hessiana indefinida ou singular.")
             return x
-        
+
         x = x + d
 
     return f(x), x
